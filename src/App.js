57f9eeb0e49d9315615fa43a4e6e5596a7311d3b
abdullah-jaffer <abdullahjaffer96@gmail.com>
2021-08-16 01:05:25 +0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useDarkMode } from './customhooks/ThemeHook';
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '10px 60px 10px 60px', backgroundColor: darkMode && '#282c34' }}>
+      <h1 style={{ color: darkMode && 'white' }}>{darkMode ? 'Dark Theme' : 'Light Theme'}</h1>
+      <p style={{ color: darkMode && 'white' }}>Wow this is awesome!</p>
+      <button
+        onClick={() => setDarkMode(!darkMode)}
+        style={{ color: darkMode && 'white', background: darkMode && '#db7093' }}
+      >
+        {darkMode ? 'Select Light Theme' : 'Select Dark Theme'}
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ThemeProvider from './context/ThemeProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
