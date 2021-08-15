efa6a19a04c06b4f355c95e546eb8b8c23d5e369
abdullah-jaffer <abdullahjaffer96@gmail.com>
2021-08-16 03:55:59 +0500

    added full screen darkmode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useDarkMode } from './customhooks/ThemeHook';
 import './App.css';
-
 function App() {
   const [darkMode, setDarkMode] = useDarkMode(false);
   return (
-    <div style={{ padding: '10px 60px 10px 60px', backgroundColor: darkMode && '#282c34' }}>
+    <div className="full-screen" style={{ backgroundColor: darkMode && '#282c34' }}>
       <h1 style={{ color: darkMode && 'white' }}>{darkMode ? 'Dark Theme' : 'Light Theme'}</h1>
       <p style={{ color: darkMode && 'white' }}>Wow this is awesome!</p>
       <button
